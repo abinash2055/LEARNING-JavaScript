3174c49751b2549d9a63ffea70c9e2c66f7432dd
Abinash Nath Pandey <abinashnathpandey@gmail.com>
2024-10-24 07:43:40 +0545

    Currency

---
+++ DAY 13/script.js
@@ -0,0 +1,6 @@
+// Fetch API --> provides interface for fetching resources.
+// uses request and response object --> fetch method is used to fetch a data
+// API ---> full form is Application Programming Interface
+
+
+const URL = "https://cat-fact.herokuapp.com/facts";
